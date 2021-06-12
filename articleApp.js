0623f5464a0b8d782bfc1e87e1c5163d725260aa
Emilien Vaschalde <emilienvaschalde@yahoo.fr>
2021-06-12 10:12:56 +0200

    page article spe - bouton acheter non fonctionnel

--- articleApp.js
+++ articleApp.js
@@ -8,16 +8,42 @@ async function main() {
     let nom = articles[articleId].name
     let imageUrl = articles[articleId].imageUrl
     let description = articles[articleId].description
+    
+    let lentilleTab = articles[articleId].lenses
+    let lentilleHtml = makeLentilleHtml(lentilleTab)
 
     document.getElementById("arianeArticle").innerText = nom
     document.getElementById("titre").innerText = nom
     document.getElementById("image").innerHTML = `<img src="${imageUrl}" id="imageAppareil" alt="Photo du ${nom}, appareil photo d'Orinoco">`
+    
     document.getElementById("description").innerText = description
+    document.getElementById("lentille").innerHTML = lentilleHtml
     
     //retouche du cadrage spécifiquement pour un appareil
     if (nom == "Hirsch 400DTS") {
         document.getElementById("imageAppareil").classList.add("imagArticle--positionHirsh")
     }
+
+
+    //fais le code html de la liste
+    function makeLentilleHtml(tab) {
+
+        let html = `<option value="">${tab[0]}</option>`
+
+        for (i=1 ; i < tab.length ; i++) {
+            
+            html = html + `<option value="">${tab[i]}</option>`
+        }
+        return html
+    }
+
+
+
+    //Acheter
+    
+    document.getElementById("acheter").addEventListener("click", function() {
+        console.log("acheter")
+    })
 
 }
 
